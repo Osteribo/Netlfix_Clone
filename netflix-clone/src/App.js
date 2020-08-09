3d64f4455e676c5d6d0a21a2a38d3c814eb383cf
ALO <36780370+Osteribo@users.noreply.github.com>
2020-08-08 21:53:44 -0700

    Update: Rows are generated

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row.js';
 
 function App() {
   return (
     <div className="App">
       <h1> Heyo Lets Code it up!</h1>
+
+      <Row title="Netflix Originals"></Row>
+
+      <Row title="Trending now"></Row>
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,18 @@
+// this is an component hence the the capital R in Row.js
+// this will create the rows and sections for the api to fill with shows/movies
+
+
+import React from 'react'
+
+function Row({title}) {
+    return (
+        <div>
+            
+            <h2> {title}</h2>
+
+
+        </div>
+    )
+}
+
+export default Row
