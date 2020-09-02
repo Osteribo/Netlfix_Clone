89eaa9bc0cb9ab43c07c4b98fe8a6f0889490dfa
ALO <36780370+Osteribo@users.noreply.github.com>
2020-09-02 16:51:34 -0700

    Update: Async call to pull movies from server

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Row from './Row.js';
+import requests from './requests.js';
 
 function App() {
   return (
     <div className="App">
       <h1> Heyo Lets Code it up!</h1>
 
-      <Row title="Netflix Originals"></Row>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}></Row>
+      
 
-      <Row title="Trending now"></Row>
+      <Row title="Trending now" fetchUrl={requests.fetchTrending}></Row>
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,9 +2,31 @@
 // this will create the rows and sections for the api to fill with shows/movies
 
 
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from './axios';
 
-function Row({title}) {
+// useState for short term vairiables. They will empty on refresh
+
+function Row({title, fetchUrl}) {
+        // short term holder for movies to populate
+    const [movies, setMovies] = useState([]);
+
+        // each time the row is loaded this is called to fill the rows with movies.
+        
+    useEffect(() => {
+        // in order to use async inside of useEffect you need to make it a function. 
+        async function fetchData() {
+                // create a variable to store response from server
+            const request = await axios.get(fetchUrl);
+                // check that there are results
+            console.log(request.data.results);
+                // returnt the variable with data to the row
+            return request;
+        }
+        fetchData();
+        // if [] contain a variable then it will load anytime the variable changes. if [] is empty then it only runs once per Row call
+    }, [])
+
     return (
         <div>
             
