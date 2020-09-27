353fbb78dee85e809513923236eae5e4ce9af915
ALO <36780370+Osteribo@users.noreply.github.com>
2020-09-27 11:52:23 -0700

    Fix: corrected API call for top_rated

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -6,16 +6,14 @@ const API_KEY = "4d39fff22390e1871bfb88ccc0ac8426";
 // a dictionary of endpoints to use with API key to ping TMDB's database
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movies/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovie: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovie: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovie: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovie: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovie: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovie: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
-}
-
-export default requests
+export default requests;
