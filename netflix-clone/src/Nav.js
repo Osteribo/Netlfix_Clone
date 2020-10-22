5e88a7cc96d98b4a88f27ce88d13fec63fcbeaa2
ALO <36780370+Osteribo@users.noreply.github.com>
2020-10-21 20:56:56 -0700

    Update: Added black bar to nav on Y-axis scroll

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, setshow] = useState(false);
+  // this addes a black bar when the user scrolls 100px or more in the Y direction
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setshow(true);
+      } else setshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
